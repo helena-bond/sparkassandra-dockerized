9aa3efe62d59496b33af03edd548d5d4caa3de7e
bert <bpoller@ekito.fr>
2016-06-03 14:49:16 +0200

    Implemented CSV to dataframe loading function.

--- src/main/java/io/ekito/sparktest/IngestionJob.java
+++ src/main/java/io/ekito/sparktest/IngestionJob.java
@@ -1,15 +1,30 @@
 package io.ekito.sparktest;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
 
 public class IngestionJob {
 
+    SQLContext sqlContext;
 
+    public IngestionJob() {
+        SparkConf conf = new SparkConf()
+                .setAppName("Data Ingestion job")
+                .set("spark.cassandra.connection.host", "127.0.0.1")
+                .setMaster("local");
 
-    public IngestionJob(){
-        //SparkConf conf = new SparkConf().setAppName( "Data Ingestion job");
-        //SparkContext sc = new SparkContext(conf);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sqlContext = new SQLContext(sc);
     }
 
+    public DataFrame loadCSVToDataFrame(String filePath) {
+        return sqlContext
+                .read()
+                .format("com.databricks.spark.csv")
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .load(filePath);
+    }
 }
--- src/test/java/io/ekito/sparktest/IngestionJobTest.java
+++ src/test/java/io/ekito/sparktest/IngestionJobTest.java
@@ -2,10 +2,15 @@ package io.ekito.sparktest;
 
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import org.apache.spark.sql.DataFrame;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.stream.StreamSupport;
 
 public class IngestionJobTest {
 
@@ -16,11 +21,12 @@ public class IngestionJobTest {
 
     @Before
     public void init() {
-        job = new IngestionJob();
         Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
         session = cluster.connect();
         session.execute("create keyspace test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
-        session.execute("CREATE TABLE test.basket (     \"Id\"         INT PRIMARY KEY,     \"Player\"           text,     \"Number\"        INT )  ;");
+        session.execute("CREATE TABLE test.basket (\"Id\" INT PRIMARY KEY, \"Player\" text, \"Number\" INT)");
+
+        job = new IngestionJob();
     }
 
     @After
@@ -30,9 +36,14 @@ public class IngestionJobTest {
 
 
     @Test
-    public void shouldImportCorrectly() {
+    public void shouldLoadCSVToDataFrame() {
 
+        DataFrame dataFrame = job.loadCSVToDataFrame("src/test/ressources/basket.csv");
 
+        Assert.assertEquals(3L, dataFrame.count());
+
+        //ResultSet result = session.execute("select * from test.basket");
+        //Assert.assertEquals(3L, StreamSupport.stream(result.spliterator(), false).count());
     }
 
 }

57ecd02dff7b35978b7c3382926245213deac34f
alexia audevart <aaudevart@ekito.fr>
2016-11-10 16:07:14 +0100

    token

--- swarm_cluster/create_vbox_cluster.sh
+++ swarm_cluster/create_vbox_cluster.sh
@@ -8,31 +8,40 @@
 #Creates machines in default project previously initialized in gcloud
 
 # Clean any existing machines
-yes | docker-machine rm local -y -f
+yes | docker-machine rm vbox-consul -y -f
 yes | docker-machine rm swarm-manager -y -f
-yes | docker-machine rm node-01 -y -f
+for MGR_ID in {1..3}
+do
+    yes | docker-machine rm node-$MGR_ID -y -f
+done
 
-# Create machine (kv store)
-docker-machine create --driver virtualbox local
+# Create machine consul (kv store)
+docker-machine create --driver virtualbox vbox-consul
 
-# Get SWARM token
-eval $(docker-machine env local)
-SWARM_CLUSTER_TOKEN="$(docker run --rm swarm create)"
+eval $(docker-machine env vbox-consul)
 
-# Build swarm-manager
-docker-machine create \
-    -d virtualbox \
-    --swarm \
-    --swarm-master \
-    --swarm-discovery token://$SWARM_CLUSTER_TOKEN \
-    swarm-manager
+docker run -d -p 8500:8500 \
+--restart always --name=consul \
+progrium/consul -server -bootstrap
 
-# Build node-01
-docker-machine create \
-    -d virtualbox \
-    --swarm \
-    --swarm-discovery token://$SWARM_CLUSTER_TOKEN \
-    node-01
+# Get IP
+CONSUL_IP=$(docker-machine ip vbox-consul-1)
+echo Consul Server up and running. IP: $CONSUL_IP
+
+#create swarm managers
+for MGR_ID in {1..3}
+do
+  docker-machine create \
+  --driver virtualbox \
+  --swarm \
+  --swarm-master \
+  --swarm-discovery consul://$CONSUL_IP:8500 \
+  --engine-opt cluster-store=consul://$CONSUL_IP:8500 \
+  --engine-opt cluster-advertise=eth1:2376 \
+  vbox-mgr-$MGR_ID
+done
+
+
 
 #
 #docker run -d -p 8500:8500 \

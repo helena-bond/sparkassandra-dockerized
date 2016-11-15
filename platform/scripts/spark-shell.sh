7c93784a54121775a3f57c808f95e2bb24ff3d72
franck <ftheroude@ekito.fr>
2016-11-15 19:29:55 +0100

    open spark port, use home build image, change readme.md

--- platform/scripts/spark-shell.sh
+++ platform/scripts/spark-shell.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
+
 export SPARK_LOCAL_IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+
+echo SPARK_LOCAL_IP is : $SPARK_LOCAL_IP
+
+echo spark url is  : $(host platform_spark_master_1 | awk '/has address/ { print $4 }')
+
 cd /usr/local/spark
 ./bin/spark-shell \
-	--master spark://$(host spark_master | awk '/has address/ { print $4 }'):7077   \
+	--master spark://$(host platform_spark_master_1 | awk '/has address/ { print $4 }'):7077   \
 	--conf spark.driver.host=${SPARK_LOCAL_IP} \
 	--properties-file /spark-defaults.conf \
 	--jars /spark-cassandra-connector-assembly-1.6.0-M2.jar\
-	--conf spark.cassandra.connection.host=$(host cassandra_master | awk '/has address/ { print $4 }') \
+	--conf spark.cassandra.connection.host=$(host platform_cassandra_master_1 | awk '/has address/ { print $4 }') \
 	"$@"
--- swarm_cluster/create_cluster.sh
+++ swarm_cluster/create_cluster.sh
@@ -15,8 +15,9 @@ gcloud auth activate-service-account --key-file ~/Share/DOCKER_SHARED/sparkassan
 #Allow ssh, Swarm is able to talk to its instances
 gcloud compute firewall-rules create default-ssh --allow tcp:22,icmp --source-ranges 0.0.0.0/0 --project=$PROJECT_ID
 gcloud compute firewall-rules create default-swarm --allow tcp:2376,tcp:3376 --source-ranges 0.0.0.0/0 --project=$PROJECT_ID
-gcloud compute firewall-rules create default-consul --allow tcp:8500,tcp:8500 --source-ranges 0.0.0.0/0 --project=$PROJECT_ID
-
+gcloud compute firewall-rules create default-consul --allow tcp:8500 --source-ranges 0.0.0.0/0 --project=$PROJECT_ID
+gcloud compute firewall-rules create default-spark --allow tcp:4040,tcp:6066,tcp:7000,tcp:7001,tcp:7002,tcp:7003,tcp:7004,tcp:7005,tcp:7006,tcp:7077,tcp:7199,tcp:8080,tcp:8081,tcp:8888,tcp:9042,tcp:9160  --source-ranges 0.0.0.0/0 --project=$PROJECT_ID
+gcloud compute firewall-rules create default-cassandra --allow tcp:7199 --source-ranges 0.0.0.0/0 --project=$PROJECT_ID
 
 #CREATE KEY-VALUE STORE FOR DISCOVERY
 #create consul kv store

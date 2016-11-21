9082ae778af70be19fc5fcc052da6eb36cd78deb
franck <ftheroude@ekito.fr>
2016-11-21 18:32:26 +0100

    add cassandra scheme init

---
+++ swarm_cluster/cassandraSpark.sql
@@ -0,0 +1,42 @@
+create keyspace IF NOT EXISTS sparkassandra WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+CREATE TABLE sparkassandra.us_flights (
+  Id                text PRIMARY KEY,
+  Year              text,
+  Month             text,
+  DayOfMonth       text,
+  DayOfWeek         text,
+  DepTime           text,
+  CRSDepTime        text,
+  ArrTime           text,
+  CRSArrTime        text,
+  UniqueCarrier     text,
+  FlightNum         text,
+  TailNum           text,
+  ActualElapsedTime text,
+  CRSElapsedTime    text,
+  AirTime           text,
+  ArrDelay          text,
+  DepDelay          text,
+  Origin            text,
+  Dest              text,
+  Distance          text,
+  TaxiIn            text,
+  TaxiOut           text,
+  Cancelled         text,
+  CancellationCode  text,
+  Diverted          text,
+  CarrierDelay      text,
+  WeatherDelay      text,
+  NASDelay          text,
+  SecurityDelay     text,
+  LateAircraftDelay text
+);
+CREATE TABLE sparkassandra.airports (
+  iata              text PRIMARY KEY,
+  airport           text,
+  city              text,
+  state             text,
+  country           text,
+  lat               text,
+  long              text
+);

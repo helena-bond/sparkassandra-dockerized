7715fe83be19ab66178e75e05d45e7afdae76ef7
bert <bpoller@ekito.fr>
2016-11-22 10:22:50 +0100

    Added instance group stuff.

--- swarm_cluster/create_cluster.sh
+++ swarm_cluster/create_cluster.sh
@@ -2,6 +2,8 @@
 
 # recovers the default project id
 PROJECT_ID="sparkassandrito"
+MASTER_COUNT=1
+NODE_COUNT=3
 
 # sets up google api credentials location
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/sparkassandrito-7cedd904e9fc.json"
@@ -13,7 +15,6 @@ docker-machine create \
 --google-zone europe-west1-b \
 --google-project $PROJECT_ID \
 gce-consul-1
-
 
 #redirect docker-ci on consul machine to deploy consul image
 eval $(docker-machine env gce-consul-1)
@@ -30,6 +31,8 @@ echo Consul Server up and running. IP: $CONSUL_IP
 #create swarm cluster relying on CONSUL discovery
 
 #create swarm master
+for MASTER_ID in {1..$MASTER_COUNT}
+do
  docker-machine create \
   --driver google \
   --google-zone europe-west1-b \
@@ -41,12 +44,11 @@ echo Consul Server up and running. IP: $CONSUL_IP
   --engine-opt cluster-store=consul://$CONSUL_IP:8500 \
   --engine-opt="cluster-advertise=eth0:2376" \
   gce-master-1 &
-
-
+done
 
 #create swarm node
 # expose http8080 for spark master capabilities
-for MGR_ID in {1..3}
+for NODE_ID in {1..$NODE_COUNT}
 do
   docker-machine create \
   --driver google \
@@ -59,11 +61,23 @@ do
   --engine-label sparkport=http8080 \
   --engine-label jupyterport=http8890 \
   --google-tags http8080,http8890 \
-  gce-node-$MGR_ID &
+  gce-node-$NODE_ID &
 done
 
-
 wait
+
+# Add nodes to instance group for reverse proxy and load balancing
+for MASTER_ID in {1..$MASTER_COUNT}
+do
+ gcloud compute instance-groups unmanaged add-instances sparkcluster --instances gce-master-$MASTER_ID --zone europe-west1-b
+done
+
+for NODE_ID in {1..$NODE_COUNT}
+do
+ gcloud compute instance-groups unmanaged add-instances sparkcluster --instances gce-master-$NODE_ID --zone europe-west1-b
+done
+
+#Some checks
 
 #list machines
 docker-machine ls
@@ -75,6 +89,3 @@ docker info
 
 #TO TRY
 #docker run swarm list consul://$(docker-machine ip consul-machine):8500
-
-
-

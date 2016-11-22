b0f0c85c4ddc93e89f17794dd581e16a8c3acfc9
franck <ftheroude@ekito.fr>
2016-11-22 01:30:32 +0100

    integrate three spark worker

--- swarm_cluster/add_worker.sh
+++ swarm_cluster/add_worker.sh
@@ -16,7 +16,7 @@ CONSUL_IP=$(docker-machine ip gce-consul-1)
 
 #create swarm node
 # expose http8080 for spark master capabilities
-for MGR_ID in {4..6}
+for MGR_ID in {4..7}
 do
   docker-machine create \
   --driver google \
